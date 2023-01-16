d1cc4face2b4a56faab99c0bf774b87092727057
Kunal Singh <66094420+94kunal@users.noreply.github.com>
2023-01-16 12:12:55 +0100

    Update MQTT_Kafka_Bridge.py

--- MQTT/MQTT_Kafka_Bridge.py
+++ MQTT/MQTT_Kafka_Bridge.py
@@ -1,20 +1,12 @@
 import json
 import paho.mqtt.client as client
-import yaml
 import pathlib
 import time
+from utilities import yaml_loadfile
 from kafka import KafkaProducer
 
 
-def yaml_loadfile(filepath):
-
-    with open(filepath, 'r') as f:
-        data = yaml.safe_load(f)
-    return data
-
-
 def on_connect(client, userdata, flags, rc=0):
-
     if rc == 0:
         print("Connection to the MQTT-Broker is successful and Starting MQTT-KAFKA Bridge")
     else:
@@ -22,7 +14,6 @@ def on_connect(client, userdata, flags, rc=0):
 
 
 def json_serializer(data):
-
     return json.dumps(data).encode("utf-8")
 
 
@@ -36,13 +27,18 @@ class Mqtt_Kafka_Bridge:
         self.subscriber = client.Client("MQTT Kafka Bridge")
         self.subscriber.on_connect = on_connect
         self.subscriber.username_pw_set(self.details[0]['MQTT']['username'], self.details[0]['MQTT']['password'])
-        self.subscriber.connect(self.details[0]['MQTT']['broker'], self.details[0]['MQTT']['port'], keepalive=250)
-        self.subscriber.subscribe(self.details[0]['MQTT']['topic'])
 
-        self.producer = KafkaProducer(bootstrap_servers=[self.details[1]['KAFKA']['broker']],
-                                      value_serializer=json_serializer)
-        self.subscriber.on_message = self.on_message
-        self.subscriber.loop_forever()
+        try:
+            self.subscriber.connect(self.details[0]['MQTT']['broker'], self.details[0]['MQTT']['port'], keepalive=250)
+            self.subscriber.subscribe(self.details[0]['MQTT']['topic'])
+
+            self.producer = KafkaProducer(bootstrap_servers=[self.details[1]['KAFKA']['broker']],
+                                          value_serializer=json_serializer)
+            self.subscriber.on_message = self.on_message
+            self.subscriber.loop_forever()
+
+        except:
+            print("Error in MQTT-KAFKA Bridge connection")
 
     def on_message(self, client, userdata, msg):
         timestamp = int(round(time.time() * 1000))

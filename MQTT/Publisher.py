0f720daaa335be8d01191cfc292cd215b7879d31
Kunal Singh <66094420+94kunal@users.noreply.github.com>
2023-01-16 12:12:06 +0100

    Update Publisher.py

--- MQTT/Publisher.py
+++ MQTT/Publisher.py
@@ -1,37 +1,13 @@
 import serial
 import sys
-import yaml
 import time
 import json
 import pathlib
+from utilities import yaml_loadfile
 import paho.mqtt.client as client
 from kafka import KafkaConsumer
 from threading import *
-
-
-def yaml_loadfile(filepath):
-    with open(filepath, 'r') as f:
-        data = yaml.safe_load(f)
-    return data
-
-
-def heater_on_command():
-    hex_array = []
-    heater_on = 0x00
-    msg = [0x00, 0x0, 0x0]
-    hex_array.insert(0, heater_on)
-    command_to_send = []
-    for i in range(1, 4):
-        hex_array.append(msg[i - 1])
-    fill_byte = sum(hex_array) + -sum(hex_array)
-    hex_array.insert(4, fill_byte)
-    checksum = 256 - sum(hex_array)
-    hex_array.insert(5, checksum)
-    for i in hex_array:
-        command_to_send.append(hex(i))
-    result = bytes([int(x, 0) for x in command_to_send])
-    return result
-
+import time
 
 def on_connect(client, userdata, flags, rc=0):
     if rc == 0:
@@ -41,7 +17,6 @@ def on_connect(client, userdata, flags, rc=0):
 
 
 def get_lastbit(lastbit):
-
     if lastbit < 0:
         lastbit = lastbit + 256
         return lastbit
@@ -59,64 +34,49 @@ class Mqtt_Publisher:
         self.publisher = client.Client("MQTT Python Publisher")
         self.publisher.on_connect = on_connect
         self.publisher.username_pw_set(self.details[0]['MQTT']['username'], self.details[0]['MQTT']['password'])
-        self.consumer = KafkaConsumer(self.details[1]['KAFKA']['Heater_Topic'],
+
+        self.consumer = KafkaConsumer(self.details[1]['KAFKA']['topic'],
                                       bootstrap_servers=[self.details[1]['KAFKA']['broker']],
                                       auto_offset_reset='latest',
-                                      group_id="xxx"
+                                      group_id="feedback_control_loop"
                                       )
-        try:
-            self.publisher.connect(self.details[0]['MQTT']['broker'], self.details[0]['MQTT']['port'], keepalive=250)
-            self.ser = serial.Serial('xxx', 38400, timeout=1, parity=serial.PARITY_NONE,
-                                     stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
 
-            print("Established Connection with STM32 Nucleo Board")
-        except:
-            sys.exit("Error connecting device")
+        self.publisher.connect(self.details[0]['MQTT']['broker'], self.details[0]['MQTT']['port'], keepalive=250)
+        self.ser = serial.Serial('COM3', 9600, timeout=1, parity=serial.PARITY_NONE,
+                                 stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+
+        print("Established Connection with Raspberry Pi Pico Board")
 
     def get_data(self):
 
-        start_bit = [0, 0, 0, 0]
         self.publisher.loop_start()
         while True:
 
             queue = self.ser.inWaiting()
+
             if queue > 0:
-                self.data = self.ser.read(100)
+                self.data = self.ser.read(12)
                 self.ser.flushInput()
                 self.ser.flushOutput()
-                byte_data = []
-                for byte in self.data:
-                    byte_data.append(byte)
+                raw_data = [str(self.data, "UTF-8")]
 
-                if byte_data[0:4] == start_bit:
-                    timestamp = int(round(time.time() * 1000))
+                timestamp = int(round(time.time() * 1000))
 
-                    "Get the sensor Decimal values and  decoding it to Temperature and Humidity Values"
+                "Get the sensor Decimal values and  decoding it to Temperature and Humidity Values"
 
-                    Ambient_Temperature: float = (byte_data[10] * 256 + get_lastbit(byte_data[11])) / 100
+                Ambient_Temperature: float = eval(raw_data[0][0:4])
 
-                    Ambient_Humidity: float = (byte_data[12] * 256 + get_lastbit(byte_data[13])) / 100
+                Ambient_Humidity: float = eval(raw_data[0][6:10])
 
-                    Wall1_Temperature: float = (byte_data[14] * 256 + get_lastbit(byte_data[15])) / 100
+                Sensor_Data = {"Ambient Temperature": Ambient_Temperature, "Ambient Humidity": Ambient_Humidity,
+                               "MQTT_Timestamp": timestamp}
 
-                    Wall1_Humidity: float = (byte_data[16] * 256 + get_lastbit(byte_data[17])) / 100
+                print(Sensor_Data)
+                Sensor_Data_json = json.dumps(Sensor_Data)
 
-                    Wall2_Temperature: float = (byte_data[18] * 256 + get_lastbit(byte_data[19])) / 100
+                self.publisher.publish(self.details[0]['MQTT']['topic'], Sensor_Data_json)
 
-                    Wall2_Humidity: float = (byte_data[20] * 256 + get_lastbit(byte_data[21])) / 100
-
-                    Sensor_Data = {"Ambient Temperature": Ambient_Temperature, "Ambient Humidity": Ambient_Humidity,
-                                   "Wall1 Temperature": Wall1_Temperature, "Wall1 Humidity": Wall1_Humidity,
-                                   "Wall2 Temperature": Wall2_Temperature, "Wall2 Humidity": Wall2_Humidity,
-                                   "MQTT_Timestamp": timestamp}
-
-                    print(Sensor_Data)
-                    Sensor_Data_json = json.dumps(Sensor_Data)
-
-                    self.publisher.publish(self.details[0]['MQTT']['topic'], Sensor_Data_json)
-
-                self.data = 0
-                print()
+            self.data = 0
             time.sleep(0.2)
             self.publisher.loop_stop()
 
@@ -124,13 +84,16 @@ class Mqtt_Publisher:
         while True:
             for msg in self.consumer:
                 Sensor_Data = json.loads(msg.value)
-                if Sensor_Data['xxx'] == 1:
-                    self.ser.write(heater_on_command())
+                if Sensor_Data['Ambient Temperature'] >= 24.0:
+                    # self.ser.write(b"on\n")
+                    # time.sleep(1)
+                    # self.ser.write(b"off\n")
+                    # time.sleep(1)
+                    # self.ser.write(heater_on_command())
                     print("HEATER TURNING ON AFTER 1 MINUTE!!")
-
 
 Mqtt_Publisher_object = Mqtt_Publisher()
 t1 = Thread(target=Mqtt_Publisher_object.get_data)
-t2 = Thread(target=Mqtt_Publisher_object.feedback_control)
 t1.start()
+t2 = Thread(target=Mqtt_Publisher_object.feedback_control)
 t2.start()
